20399797f063322374eb682601d33258c55abf88
SiYue0501 <318001762@qq.com>
2023-03-25 19:38:36 +0800

    4

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template("login.html")
 
 
 if __name__ == '__main__':

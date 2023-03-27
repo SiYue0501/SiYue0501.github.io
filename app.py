bc03a6bad6dc45496aeb7936ea6e19f6d4034dbf
SiYue0501 <318001762@qq.com>
2023-03-27 16:53:44 +0800

    1

--- app.py
+++ app.py
@@ -10,5 +10,18 @@ def hello_world():  # put application's code here
     return render_template("login.html")
 
 
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        name = request.form["user"]
+        pwd = request.form["password"]
+        if name == "11" and pwd == "11":
+            return render_template("shouye.html")
+        else:
+            return render_template("login.html", msg="密码错误")
+    else:
+        return render_template("404.html")
+
+
 if __name__ == '__main__':
     app.run()
